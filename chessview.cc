a88da650dd2e5e90f03509c5e60da0114c91aed7
LAPTOP-ESV4CQ7V\Cellina <patriciacellina@gmail.com>
2021-12-10 21:24:09 +0700

    updated chessview

--- chessview.cc
+++ chessview.cc
@@ -1,5 +1,6 @@
 #include "chessview.h"
 #include "textobserver.h"
+#include "graphicsobserver.h"
 #include <iostream>
 
 using namespace std;
@@ -14,84 +15,51 @@ void ChessView::run()
 	observers.push_back(make_shared<TextObserver>(board));
 	graphic_observers.push_back(make_shared<GraphicsObserver>(board));
 	ChessController controller = ChessController(board);
-	while (cin >> command){
-		if (command == "game"){
-			try {
+	while (cin >> command)
+	{
+		if (command == "game")
+		{
+			try
+			{
 				string p1;
 				string p2;
 				cin >> p1 >> p2;
 				controller.start(p1, p2);
-			} catch (GameError ge) {
-                		cout << "game error: " << ge.get_message() << endl;
-        		}
-		}
-		if (command == "move"){
-			try {
-				string move_commands;
-				getline(cin, move_commands);
-				controller.move(move_commands);
-			} catch (GameError ge) {
-                		cout << "game error: " << ge.get_message() << endl;
-        		}	
-		}
-		if (command == "undo") {
-			try {
-				controller.undo();
-			} catch (GameError ge) {
-                                cout << "game error: " << ge.get_message() << endl;
-                        }
-		}
-		if (command == "resign") {
-			try {
-				controller.resign();
-			} catch (GameError ge) {
-                                cout << "game error: " << ge.get_message() << endl;
-                        }
-		}
-		if (command == "setup") {
-			try {
-				controller.setup();
-			} catch (GameError ge) {
-                                cout << "game error: " << ge.get_message() << endl;
-                        }
-		}
-		if (command == "+") {
-			try {
-				char piece;
-				string position;
-				cin >> piece >> position;
-				controller.setup_add(piece, position);
-				board->notifyObservers("n");
-			} catch (GameError ge) {
-                                cout << "game error: " << ge.get_message() << endl;
-                        }
-		}
-		if (command == "-") {
-			try {
-                                string position;
-				cin >> position;
-                                controller.setup_remove(position);
-				board->notifyObservers("n");
-                        } catch (GameError ge) {
-                                cout << "game error: " << ge.get_message() << endl;
-                        }
-		}
-		if (command == "=") {
-			try {
-				char color;
-				cin >> color;
-				controller.setup_color(color);
-				board->notifyObservers("n");
-			} catch(GameError ge) { 
+			}
+			catch (GameError ge)
+			{
 				cout << "game error: " << ge.get_message() << endl;
 			}
 		}
-		if (command == "done") {
-			try {
-				controller.setup_done();
-			} catch(GameError ge) {
-                                cout << "game error: " << ge.get_message() << endl;
-                        }
+		if (command == "move")
+		{
+			try
+			{
+				string move_commands;
+				getline(cin, move_commands);
+				controller.move(move_commands);
+			}
+			catch (GameError ge)
+			{
+				cout << "game error: " << ge.get_message() << endl;
+			}
+		}
+		if (command == "undo")
+		{
+			try
+			{
+				controller.undo();
+			}
+			catch (GameError ge)
+			{
+				cout << "game error: " << ge.get_message() << endl;
+			}
+		}
+		if (command == "resign")
+		{
+			// TO DO: find which player concedes defeat
+			// std::cout << "the #COLOUR player concedes defeat. #OPPONENT_COLOUR wins"
+			return;
 		}
 	}
 }
